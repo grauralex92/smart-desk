5952411a699f8e26df074459496ee7081c2429f0
Alex Graur <grauralex@gmail.com>
2019-06-11 15:33:34 +0300

    added welcome screen

---
+++ app/src/main/java/com/endava/smartdesk/UserData.java
@@ -0,0 +1,72 @@
+package com.endava.smartdesk;
+
+public class UserData {
+
+    private String firstName;
+    private String lastName;
+    private String companyName;
+    private String email;
+    private String purpose;
+    private long arrivalDate;
+    private long departureDate;
+
+    public UserData() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public long getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(long arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public long getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(long departureDate) {
+        this.departureDate = departureDate;
+    }
+}
