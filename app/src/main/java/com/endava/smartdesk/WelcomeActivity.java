154e879fe9c22c82226f6a60d2a02cbdd3f59b79
Gabriel Blaj <gabriel.blaj@endava.com>
2019-06-13 14:46:33 +0300

    WIP: User Interface

--- app/src/main/java/com/endava/smartdesk/WelcomeActivity.java
+++ app/src/main/java/com/endava/smartdesk/WelcomeActivity.java
@@ -1,6 +1,9 @@
 package com.endava.smartdesk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +16,14 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         ImageView imageView = findViewById(R.id.welcome_background_image_view);
+        Button registerNowButton = findViewById(R.id.register_now_button);
+        Button alreadyRegisteredButton = findViewById(R.id.already_registered_button);
+
+        registerNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
